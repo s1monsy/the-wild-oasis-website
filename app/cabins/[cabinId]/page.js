af9f2e7a136938fe8b7e510d3cc149ba86b5cd77
DenysPolupan <polupan2004denis@gmail.com>
2026-04-01 13:52:10 +0300

    Pre-render cabin pages, add revalidate and prod script

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -7,12 +7,16 @@ export async function generateMetadata({ params }) {
   return { title: `Cabin ${name}` };
 }
 
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  return ids;
+}
+
 export default async function Page({ params }) {
   const cabin = await getCabin(params.cabinId);
 
   const { id, name, maxCapacity, regularPrice, discount, image, description } = cabin;
-
-  console.log(params);
 
   return (
     <div className="max-w-6xl mx-auto mt-8">
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -6,6 +6,8 @@ export const metadata = {
   title: "Cabins",
 };
 
+export const revalidate = 3600;
+
 export default function Page() {
   return (
     <div>
