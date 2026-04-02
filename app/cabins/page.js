3becfbf70b3e9fcdde28d785848d11b2f5c482f1
DenysPolupan <polupan2004denis@gmail.com>
2026-04-02 14:47:39 +0300

    Add filter, text expander, and profile form

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,19 @@
 import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
 
 export const metadata = {
   title: "Cabins",
 };
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
-export default function Page() {
+export default function Page({ searchParams }) {
+  console.log(searchParams);
+
+  const filter = searchParams?.capacity ?? "all";
+
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">Our Luxury Cabins</h1>
@@ -20,8 +25,12 @@ export default function Page() {
         paradise.
       </p>
 
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8">
+        <Filter />
+      </div>
+
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
       </Suspense>
     </div>
   );
